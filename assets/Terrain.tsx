3c5edd93ba5c0a908fdbd24641360de8ff2159b9
Elvin <enarimanov22@gmail.com>
2025-12-06 14:34:50 +0100

    Add TowerDefence project

---
+++ assets/Terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Terrain" tilewidth="32" tileheight="32" tilecount="112" columns="14">
+ <image source="../../../OIP.webp" width="474" height="266"/>
+</tileset>
---
+++ assets/arbre.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="arbre" tilewidth="32" tileheight="32" tilecount="42" columns="6">
+ <image source="../../../../../Downloads/assets/assets/props.png" width="203" height="247"/>
+</tileset>
