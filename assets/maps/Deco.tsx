1258fdc19fe3d8c999433fdbce6ea69568635a9a
Louiscieb <Louis.ciebiera@gmail.com>
2026-01-12 01:31:13 +0100

    Modif de la map

---
+++ assets/maps/Deco.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="Deco" tilewidth="38" tileheight="38" tilecount="144" columns="12">
+ <image source="Deco.png" trans="00aa00" width="474" height="474"/>
+</tileset>
---
+++ assets/maps/Regles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="Regles" tilewidth="32" tileheight="32" tilecount="1457" columns="47">
+ <image source="Regles.png" trans="00aa00" width="1515" height="996"/>
+</tileset>
